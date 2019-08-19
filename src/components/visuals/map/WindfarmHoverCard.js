82f4a4351dba61ec7b73d32b4ca6bb834a4d994c
Jake Angell <jake.angell@unasys.com>
2019-08-19 09:14:07 +0100

    add windfarm hover card

--- src/components/visuals/map/WindfarmHoverCard.js
+++ src/components/visuals/map/WindfarmHoverCard.js
@@ -16,6 +16,28 @@ class WindfarmHoverCard extends Component {
                         <div className="installation-hover-card-value">{hoveredWindfarm["NAME"]}</div>
                     </div>
                 </div>
+                <div className="installation-hover-card-body">
+                <div className="text-block-container">                    
+                        <div className="installation-text-value">
+                            <div className="installation-hover-card-heading">Build Cost</div>
+                            <div className="installation-hover-card-value">{hoveredWindfarm["BUILD COST"]}</div>
+                        </div>
+                </div>
+                <div className="text-block-container">                    
+                        <div className="installation-text-value">
+                            <div className="installation-hover-card-heading">Turbines</div>
+                            <div className="installation-hover-card-value">{hoveredWindfarm["TURBINES"]}</div>
+                        </div>
+                        <div className="installation-text-value">
+                            <div className="installation-hover-card-heading">Capacity Factor</div>
+                            <div className="installation-hover-card-value">{hoveredWindfarm["CAPACITY FACTOR"]}</div>
+                        </div>
+                        <div className="installation-text-value">
+                            <div className="installation-hover-card-heading">Commisioning Date</div>
+                            <div className="installation-hover-card-value">{hoveredWindfarm["COMMISSIONING DATE"]}</div>
+                        </div>
+                        </div>
+                </div>
             </div>
         )
     }
