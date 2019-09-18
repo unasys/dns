d027565dc3929117def106f360b9029f4f524882
Jake Angell <jake.angell@unasys.com>
2019-09-18 11:23:12 +0100

    makes pitch better

--- src/components/sliding-panels/panels/details-panel/north-sea-area-panel/NorthSeaAreaPanel.js
+++ src/components/sliding-panels/panels/details-panel/north-sea-area-panel/NorthSeaAreaPanel.js
@@ -34,13 +34,13 @@ class NorthSeaAreaPanel extends React.Component {
         return (
             <div className="areas">
                 {[
-                    { name: "North Sea", flyTo: {north: 55.0, east: 6.0, south: 46.0, west: -4.0, pitch: 290}}, 
-                    { name: "East of Shetland", flyTo: {north: 58.0, east: 9.0, south: 55.0, west: -10.0, pitch: 295}}, 
-                    { name: "Morcambe Bay", flyTo: { north: 54.5, east: -3, south: 50.0, west: -6, pitch: 280}}, 
-                    { name: "Northern North Sea",  flyTo: { north: 57, east: 10, south: 55.0, west: -7, pitch: 280}}, 
-                    { name: "Central North Sea", flyTo: { north: 55, east: 10, south: 54.0, west: -7, pitch: 280}}, 
-                    { name: "Southern North Sea", flyTo: { north: 54.5, east: 4, south: 50.0, west: -1.4, pitch: 280}}, 
-                    { name: "West of Shetland", flyTo: {north: 59.0, east: 0, south: 55.0, west: -8.0, pitch: 295}}
+                    { name: "North Sea", flyTo: {north: 55.0, east: 6.0, south: 46.0, west: -4.0, pitch: -65}}, 
+                    { name: "East of Shetland", flyTo: {north: 58.0, east: 9.0, south: 55.0, west: -10.0, pitch: -65}}, 
+                    { name: "Morcambe Bay", flyTo: { north: 54.5, east: -3, south: 50.0, west: -6, pitch: -80}}, 
+                    { name: "Northern North Sea",  flyTo: { north: 57, east: 10, south: 55.0, west: -7, pitch: -75}}, 
+                    { name: "Central North Sea", flyTo: { north: 55, east: 10, south: 54.0, west: -7, pitch: -82}}, 
+                    { name: "Southern North Sea", flyTo: { north: 54.5, east: 4, south: 50.0, west: -1.4, pitch: -75}}, 
+                    { name: "West of Shetland", flyTo: {north: 59.0, east: 0, south: 55.0, west: -8.0, pitch: -75}}
                 ].map(area => this.resultItem(area))}
             </div>
         )
