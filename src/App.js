64f3d0134050e125fa5d0feb3c3d7dd73fe00c44
Tony Williams <tony.williams@unasys.com>
2019-11-20 12:51:26 +0000

    app is hook with calls for data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import history from './history';
+import React, { useEffect } from 'react';
+import { Route, Switch, Router } from 'react-router-dom';
 import Map from './components/visuals/map/Map';
 import Header from './components/header/Header';
 import './App.scss';
 import OilandGas from './components/pages/oil&gas/OilandGas';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import createReducers from './reducers/reducers';
-import { routerMiddleware } from 'connected-react-router';
-import { ConnectedRouter } from 'connected-react-router';
+
 import InstallationTable from './components/pages/tables/installationTable/InstallationTable';
 import DecomYardTable from './components/pages/tables/decomYardTable/DecomYardTable';
 import PipelineTable from './components/pages/tables/pipelineTable/PipelineTable';
@@ -17,110 +12,84 @@ import WindfarmTable from './components/pages/tables/windfarmTable/WindfarmTable
 import FieldTable from './components/pages/tables/fieldsTable/FieldTable';
 import DynamicWidthPage from './components/pages/oil&gas/DynamicWidthPage';
 
-const store = createStore(
-  createReducers(history),
-  {}, // initial state
-  compose(
-    applyMiddleware(
-      routerMiddleware(history)
-    )
-  )
-);
+import { useStateValue } from './utils/state/StateProvider'
+import { fetchInstallations, fetchDecomyards, fetchFields, fetchPipelines, fetchWindfarms } from './api/Installations';
+import { fetchFact } from './api/RandomFact';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedPipeline: null,
-      selectedInstallation: null
-    }
-    this.setSelectedPipeline = this.setSelectedPipeline.bind(this);
-    this.setSelectedInstallation = this.setSelectedInstallation.bind(this);
-  }  
+const App = () => {
+  const [, dispatch] = useStateValue();
 
-  setSelectedInstallation(newInstallation) {
-    this.setState({
-      selectedInstallation: newInstallation
-    })
-  }
+  useEffect(() => {
+    fetchInstallations().then(installations =>{ dispatch({ type: "setInstallations", installations: installations })});
+    fetchDecomyards().then(decomYards =>{ dispatch({ type: "setDecomYards", decomYards: decomYards })});
+    fetchFields().then(fields =>{ dispatch({ type: "setFields", fields: fields })});
+    fetchPipelines().then(pipelines =>{ dispatch({ type: "setPipelines", pipelines: pipelines })});
+    fetchWindfarms().then(windfarms =>{ dispatch({ type: "setWindfarms", windfarms: windfarms })});
+    fetchFact().then(facts =>{ dispatch({ type: "setFacts", facts: facts })});
+  }, []);
 
-  setSelectedPipeline(newPipeline) {
-    this.setState({
-      selectedPipeline: newPipeline
-    })
-  }
+  return (
 
-  render() {
-    return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <div className="app-container">
-            <Route render={(props) => {
-              if (this.state.checkingSession) return <span></span>;
+    <Router >
+      <div className="app-container">
+        <Header />
+        <div className="content-container">
+          <Map />
+          {/* <Switch>
+            <Route path="/installations" render={(props) => {
+              return (
+                <DynamicWidthPage backgroundColor={'rgba(39, 43, 56, 0.34)'}>
+                  <InstallationTable {...props} setSelectedInstallation={this.setSelectedInstallation}></InstallationTable>
+                </DynamicWidthPage>
+              )
+            }} />
+            <Route path="/decomyards" render={(props) => {
+              return (
+                <DynamicWidthPage backgroundColor={'rgba(39, 43, 56, 0.34)'}>
+                  <DecomYardTable {...props}></DecomYardTable>
+                </DynamicWidthPage>
+              )
+            }} />
+            <Route path="/pipelines" render={(props) => {
+              return (
+                <DynamicWidthPage backgroundColor={'rgba(39, 43, 56, 0.34)'}>
+                  <PipelineTable {...props} setSelectedPipeline={this.setSelectedPipeline}></PipelineTable>
+                </DynamicWidthPage>
+              )
+            }} />
+            <Route path="/windfarms" render={(props) => {
+              return (
+                <DynamicWidthPage backgroundColor={'rgba(39, 43, 56, 0.34)'}>
+                  <WindfarmTable {...props}></WindfarmTable>
+                </DynamicWidthPage>
+              )
+            }} />
+            <Route path="/fields" render={(props) => {
+              return (
+                <DynamicWidthPage backgroundColor={'rgba(39, 43, 56, 0.34)'}>
+                  <FieldTable {...props}></FieldTable>
+                </DynamicWidthPage>
+              )
+            }} />
+          </Switch>
+          <Switch>
+            <Route path="/(installations|decomyards|windfarms|pipelines|fields)" render={(props) => {
+              return (
+                <OilandGas {...props} changeMainContent={this.changeMainContentIndex} hideSidePanel={true}></OilandGas>
+              )
+            }} />
+            <Route path="/" render={(props) => {
+              return (
+                <OilandGas {...props} changeMainContent={this.changeMainContentIndex}></OilandGas>
+              )
+            }} />
+          </Switch> */}
 
-              return <Header {...props} />
-            }
-            } />
-            <div className="content-container">
-              <Map selectedPipeline={this.state.selectedPipeline} selectedInstallation={this.state.selectedInstallation}>
-              <Switch>          
-                <Route path="/installations" render={(props) => {
-                  return (
-                    <DynamicWidthPage backgroundColor={'rgba(39, 43, 56, 0.34)'}>
-                      <InstallationTable {...props} setSelectedInstallation={this.setSelectedInstallation}></InstallationTable>
-                    </DynamicWidthPage>
-                  )
-                }} />
-                  <Route path="/decomyards" render={(props) => {
-                  return (
-                    <DynamicWidthPage backgroundColor={'rgba(39, 43, 56, 0.34)'}>
-                      <DecomYardTable {...props}></DecomYardTable>
-                    </DynamicWidthPage>
-                  )
-                }} />
-                <Route path="/pipelines" render={(props) => {
-                  return (
-                    <DynamicWidthPage backgroundColor={'rgba(39, 43, 56, 0.34)'}>
-                      <PipelineTable {...props} setSelectedPipeline={this.setSelectedPipeline}></PipelineTable>
-                      </DynamicWidthPage>
-                      )
-                    }} />
-                <Route path="/windfarms" render={(props) => {
-                  return (
-                    <DynamicWidthPage backgroundColor={'rgba(39, 43, 56, 0.34)'}>
-                      <WindfarmTable {...props}></WindfarmTable>
-                    </DynamicWidthPage>
-                  )
-                }} />
-                <Route path="/fields" render={(props) => {
-                  return (
-                    <DynamicWidthPage backgroundColor={'rgba(39, 43, 56, 0.34)'}>
-                      <FieldTable {...props}></FieldTable>
-                    </DynamicWidthPage>
-                  )
-                }} />
-              </Switch>
-              <Switch>
-                <Route path="/(installations|decomyards|windfarms|pipelines|fields)" render={(props) => {
-                  return (
-                    <OilandGas {...props} changeMainContent={this.changeMainContentIndex} hideSidePanel={true}></OilandGas>
-                  )
-                }} />
-                <Route path="/" render={(props) => {
-                  return (
-                    <OilandGas {...props} changeMainContent={this.changeMainContentIndex}></OilandGas>
-                  )
-                }} />
-              </Switch>
-              </Map>
-            </div>
-          </div>
-        </ConnectedRouter>
-      </Provider>
-    );
-  }
+        </div>
+      </div>
+    </Router>
+  );
 }
-
 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import StateProvider from './utils/state/StateProvider';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = { installations: [], decomYards: [], fields: [], pipelines: [], windfarms: [], facts: [] };
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'setInstallations':
+            return {
+                ...state,
+                installations: action.installations
+            };
+        case 'setDecomYards':
+            return {
+                ...state,
+                decomYards: action.decomYards
+            };
+        case 'setFields':
+            return {
+                ...state,
+                fields: action.fields
+            };
+        case 'setPipelines':
+            return {
+                ...state,
+                pipelines: action.pipelines
+            };
+        case 'setWindfarms':
+            return {
+                ...state,
+                windfarms: action.windfarms
+            };
+        case 'setFacts':
+            return {
+                ...state,
+                facts: action.facts
+            };
+        default:
+            return state;
+    }
+};
+
+ReactDOM.render(
+    <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+    </StateProvider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
