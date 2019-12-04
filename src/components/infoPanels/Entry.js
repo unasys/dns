4bb377e48337bf013019107aea853c60dadbea58
Tony Williams <tony.williams@unasys.com>
2019-12-04 10:09:55 +0000

    summary

--- src/components/infoPanels/Entry.js
+++ src/components/infoPanels/Entry.js
@@ -1,21 +1,21 @@
-import "./Entry.scss"; 
-import React from 'react'; 
+import "./Entry.scss";
+import React from 'react';
 
 function Entry(props) {
     return (
-        <div className={"entry " + (props.borderBottom && "dotted-border-bottom")}>
-        {props.icon && 
-        <div className="entry-icon">
-            {props.icon}
-        </div>}
-        <div className="entry-container">
-            <div className="entry-title">
-                {props.title}
+        <div className="entry">
+            {props.icon &&
+                <div className="entry-icon">
+                    {props.icon}
+                </div>}
+            <div className="entry-content">
+                <div className="entry-title">
+                    {props.title}
+                </div>
+                <div className="entry-subtitle">
+                    {props.subtitle}
+                </div>
             </div>
-            <div className="entry-subtitle">
-                {props.subtitle}
-            </div>
-        </div>
         </div>
     )
 }
--- src/components/infoPanels/EntryContainer.js
+++ src/components/infoPanels/EntryContainer.js
@@ -1,19 +1,16 @@
 import "./EntryContainer.scss";
 import React from 'react';
-import TitleBar from "./TitleBar";
 
 function EntryContainer(props) {
-    let classes;
-    classes += (props.borderTop ? " solid-border-top" : "");
-    classes += (props.borderBottom ? " solid-border-bottom" : "");
     return (
-        <>
-        {props.title && <TitleBar title={props.title }/>}
-
-        <div className={"double-width-entry-container " + classes}>
-            {props.children}
-        </div>
-        </>
+        <details open>
+            <summary>
+                {props.title}
+            </summary>
+            <div className="entry-container">
+                {props.children}
+            </div>
+        </details>
     )
 }
 
