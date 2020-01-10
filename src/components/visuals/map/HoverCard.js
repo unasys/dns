11cec6726a3e96a506ec8ac6cc047b89dea08083
Tony Williams <tony.williams@unasys.com>
2020-01-10 14:30:41 +0000

    generic hover

--- src/components/visuals/map/HoverCard.js
+++ src/components/visuals/map/HoverCard.js
@@ -6,7 +6,38 @@ import FieldHoverCard from "./FieldHoverCard"
 import WindfarmHoverCard from "./WindfarmHoverCard"
 import SubsurfaceHoverCard from "./SubsurfaceHoverCard"
 
+const DataDrivenValue = ({ value }) => {
+    return (
+        <div className="installation-text-value">
+            <div className="installation-hover-card-heading">{value.name}</div>
+            <div className="installation-hover-card-value">{value.values}</div>
+        </div>
+    );
+}
+
+const DataDrivenHoverCard = ({ name, hoverDetails, position }) => {
+    const detailContainers = hoverDetails.map(c => <div className="text-block-container">{c.values.map(v => <DataDrivenValue value={v} />)}</div>);
+    return (
+        <div className="installation-hover-card" style={{ top: position.y, left: position.x }}>
+            <div className="installation-hover-card-title">
+                <div className="installation-text-value">
+                    <div className="installation-hover-card-heading">Name</div>
+                    <div className="installation-hover-card-value">{name}</div>
+                </div>
+            </div>
+
+            <div className="installation-hover-card-body">
+                {detailContainers}
+            </div>
+        </div>
+    )
+}
+
 const HoverCard = (props) => {
+    if (props.entity?.Hover) {
+        return <DataDrivenHoverCard position={props.position} hoverDetails={props.entity.Hover} name={props.entity.name} />
+    }
+
     switch (props.type) {
         case "Installation":
             return <InstallationHoverCard position={props.position} hoveredInstallation={props.entity} />
