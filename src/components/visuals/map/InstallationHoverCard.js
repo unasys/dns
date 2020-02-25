5665617a126e8a5c0c4db46f07d45a36a8aeca34
Tony Williams <tony.williams@unasys.com>
2020-02-25 15:36:18 +0000

    hover and info panel stlyes done

--- src/components/visuals/map/InstallationHoverCard.js
+++ src/components/visuals/map/InstallationHoverCard.js
@@ -7,52 +7,41 @@ class InstallationHoverCard extends Component {
         if (!hoveredInstallation) {
             return <div></div>;
         }
-        
+
         return (
-            <div className="hover-card" style={{top: this.props.position.y, left: this.props.position.x}}>
+            <div className="hover-card" style={{ top: this.props.position.y, left: this.props.position.x }}>
                 <div className="hover-card-title">
                     <div className="hover-text-value">
                         <div className="hover-card-heading">Field Type</div>
                         <div className="hover-card-value">{hoveredInstallation["FieldType"]}</div>
                     </div>
+                    {hoveredInstallation["ImageID"] && <img className="hover-image" src={`https://assets.digitalnorthsea.com/images/installations/${hoveredInstallation["ImageID"]}`} alt="overview-thumbnail" />}
                 </div>
                 <div className="hover-card-body">
-                    <div className="image-block-container">                    
-                            <div className="image">
-                                {hoveredInstallation["ImageID"] ? <img src={`https://assets.digitalnorthsea.com/images/installations/${hoveredInstallation["ImageID"]}`} alt="overview-thumbnail" ></img> : <img src={`https://assets.digitalnorthsea.com/images/installations/-1.jpg`} alt="overview-thumbnail" ></img>}
-                            </div>
-                        <div className="text-block">
-                            <div className="hover-text-value">
-                                <div className="hover-card-heading">Installation Name</div>
-                                <div className="hover-card-value">{hoveredInstallation["Name"]}</div>
-                            </div>
-                            <div className="hover-text-value">
-                                <div className="hover-card-heading">Block Number</div>
-                                <div className="hover-card-value">{hoveredInstallation["Block"]}</div>
-                            </div>
-                        </div>
+                    <div className="hover-text-value">
+                        <div className="hover-card-heading">Installation Name</div>
+                        <div className="hover-card-value">{hoveredInstallation["Name"]}</div>
                     </div>
-                    <div className="text-block-container">
-                        <div className="hover-text-value">
-                            <div className="hover-card-heading">Installation Type</div>
-                            <div className="hover-card-value">{hoveredInstallation["Type"]}</div>
-                        </div>
-                        <div className="hover-text-value">
-                            <div className="hover-card-heading">Operator</div>
-                            <div className="hover-card-value">{hoveredInstallation["Operator"]}</div>
-                        </div>
+                    <div className="hover-text-value">
+                        <div className="hover-card-heading">Block Number</div>
+                        <div className="hover-card-value">{hoveredInstallation["Block"]}</div>
                     </div>
-                        <div className="text-block">
-                            <div className="hover-text-value">
-                                <div className="hover-card-heading">Area</div>
-                                <div className="hover-card-value">{hoveredInstallation["Area"]}</div>
-                            </div>
-                            <div className="hover-text-value">
-                                <div className="hover-card-heading">First Oil/Gas Date</div>
-                                <div className="hover-card-value">{hoveredInstallation["StartDate"]}</div>
-                            </div>
-                        </div>
-                    
+                    <div className="hover-text-value">
+                        <div className="hover-card-heading">Installation Type</div>
+                        <div className="hover-card-value">{hoveredInstallation["Type"]}</div>
+                    </div>
+                    <div className="hover-text-value">
+                        <div className="hover-card-heading">Operator</div>
+                        <div className="hover-card-value">{hoveredInstallation["Operator"]}</div>
+                    </div>
+                    <div className="hover-text-value">
+                        <div className="hover-card-heading">Area</div>
+                        <div className="hover-card-value">{hoveredInstallation["Area"]}</div>
+                    </div>
+                    <div className="hover-text-value">
+                        <div className="hover-card-heading">First Oil/Gas Date</div>
+                        <div className="hover-card-value">{hoveredInstallation["StartDate"]}</div>
+                    </div>
                 </div>
             </div>
         )
