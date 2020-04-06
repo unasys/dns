ee44d76c6ad7261e795c34e92b24151b384f022e
Tony Williams <tony.williams@unasys.com>
2020-04-06 13:42:33 +0100

    initial dns colour changes

--- src/assets/installationTable/circle01.js
+++ src/assets/installationTable/circle01.js
@@ -25,14 +25,15 @@ export default class Circle01 extends React.Component {
     <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
         viewBox="0 0 16.8 16.8" style={{enableBackground:'new 0 0 16.8 16.8', width:this.props.size}}>
         <g>
-            <g>
-                <text transform="matrix(1 0 0 1 6.1133 10.7141)" x={xValue} style={{fill:'#EEEDED', fontFamily:'Roboto', fontSize:'6px'}}>{formattedText}</text>
-            </g>
+            
             <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="2.4634" y1="14.3577" x2="14.3577" y2="2.4634">
                 <stop  offset="0%" stopColor='#B2CD3C'/>
                 <stop  offset="100%" stopColor='#FAB71D'/>
             </linearGradient>
-            <circle style={{fill:'none', stroke:'url(#SVGID_1_)', strokeWidth:'0.5', strokeLinecap:'round', strokeLinejoin:'round', strokeMiterlimit:'10'}} cx="8.4" cy="8.4" r="8.2"/>
+            <circle style={{fill:'#FFF', stroke:'url(#SVGID_1_)', strokeWidth:'0.5', strokeLinecap:'round', strokeLinejoin:'round', strokeMiterlimit:'10'}} cx="8.4" cy="8.4" r="8.2"/>
+            <g>
+                <text transform="matrix(1 0 0 1 6.1133 10.7141)" x={xValue} style={{fill:'#000', fontFamily:'Roboto', fontSize:'6px'}}>{formattedText}</text>
+            </g>
         </g>
     </svg>
     )
