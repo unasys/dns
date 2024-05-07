0a984f208d9db57de593d05b6a241e7f293a3315
rohit-unasys <rohit.lalwani@unasys.com>
2024-05-07 22:35:45 +0100

    hacky change to accommodate a last minute request by Mick to open a link when clicking the image of a certain installation

--- src/components/infoPanels/TitleBar.js
+++ src/components/infoPanels/TitleBar.js
@@ -2,6 +2,26 @@ import "./TitleBar.scss";
 import React from 'react';
 
 function TitleBar(props) {
+    if (props.title === "CASTOR UGS PUQ" || props.title === "CASTOR UGS WHP")
+    {
+        return (
+            <div className="title-bar-container">
+                <div className="titles">
+                    <div className="main-title">
+                        {props.title}
+                    </div>
+                    <div className="main-subtitle">
+                        {props.subtitle}
+                    </div>
+                    <div>
+                        {props.epm && <img className="project-icon" src="/project.svg" alt="epm" onClick={() => window.open(`https://epm.unasys.com/projects/${props.epm}/`, "_blank")} />}
+                    </div>
+                </div>
+                {props.image && <a href="https://epm.unasys.com/projects/39660b8f-671b-4116-aea2-72b0b233c7ff/model/e6e77f4a82374c0ea845efa268f69af8" target="_blank"><img className="installation-image" src={`https://assets.digitalnorthsea.com/images/installations/${props.image}`} alt="overview-thumbnail" /></a>}
+            </div>
+        )
+    }
+
     return (
         <div className="title-bar-container">
             <div className="titles">
